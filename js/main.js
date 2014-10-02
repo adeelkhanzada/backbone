351d6d44b0eafb1541b0e301f81c23894dd241cb
Furqan Khanzada <furqan.khanzada@gmail.com>
2014-10-02 22:02:42 +0500

    create model

--- js/main.js
+++ js/main.js
@@ -1,5 +1,18 @@
 $(function(){
-    var myview = Backbone.View.extend({
+    var Mym1 = Backbone.Model.extend({
+
+        defaults:{
+            title:'Model title',
+            description:'model dummy text'
+        }
+
+});
+var m1 = new Mym1();
+
+    var data = m1.toJSON();
+   // console.log(data);
+
+    var Myview = Backbone.View.extend({
         className : 'red',
         tagName : 'section',
         attributes:{
@@ -14,7 +27,9 @@ $(function(){
         render:function(){
             var self = this;
             $.get('template/content.html',function(template){
-                var data = {title:'abc',description:'xyz'} ;
+               //var data = {title:'abc',description:'xyz'} ;
+                var data = m1.toJSON();
+                console.log(data);
                 var compiled = _.template(template);
                 self.$el.append(compiled(data));
             } );
@@ -22,14 +37,18 @@ $(function(){
             return this;
         },
         func : function(e){
-            console.log(e);
-            console.log(e.currentTarget);
+            //console.log(e);
+           // console.log(e.currentTarget);
         },
         events:{
             'click #btn' : 'func'
 
         }
     });
-    var v1 = new myview();
+    var v1 = new Myview();
     $('#container').append(v1.render().el);
+
+
 });
+
+
